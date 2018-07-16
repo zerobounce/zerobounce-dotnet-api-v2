79cb7a5b3ce979a7af5e95d00b31fb0a1cf88738
ZeroBounce <34064209+zerobounce@users.noreply.github.com>
2018-07-16 14:27:04 -0400

    Initial Code Part 2
    Initial Code Part 2

---
+++ ZeroBounce/ZeroBounceAPI.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using ZeroBounce.Models;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace ZeroBounce
+{
+
+    public class ZeroBounceAPI
+    {
+        // "Your API Key";
+        private string m_api_key = "";
+        public string api_key
+        {
+             get
+            {
+                return m_api_key;
+            }
+
+             set
+            {
+                m_api_key = value;
+            }
+        }
+        // "Your IP address";
+        private string m_ip_address = "";
+        public string ip_address
+        {
+            get
+            {
+                return m_ip_address;
+            }
+
+            set
+            {
+                m_ip_address = value;
+            }
+        }
+        private string m_emailToValidate = "";
+        public string EmailToValidate
+        {
+            get
+            {
+                return m_emailToValidate;
+            }
+
+            set
+            {
+                m_emailToValidate = value;
+            }
+        }
+
+        private int m_requestTimeOut = 100000;
+        public int RequestTimeOut
+        {
+            get
+            {
+                return m_requestTimeOut;
+            }
+
+            set
+            {
+                m_requestTimeOut = value;
+            }
+        }
+        private int m_readTimeOut = 100000;
+        public int ReadTimeOut
+        {
+            get
+            {
+                return m_readTimeOut;
+            }
+
+            set
+            {
+                m_readTimeOut = value;
+            }
+        }
+        public ZeroBounceResultsModel ValidateEmail()
+        {
+            // secure SSL / TLS channel for different .Net versions           
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11;
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+
+            var apiUrl = "";
+
+            apiUrl = "https://api.zerobounce.net/v2/validate?api_key=" + api_key + "&email=" + System.Net.WebUtility.UrlEncode(EmailToValidate) + "&ip_address=" + System.Net.WebUtility.UrlEncode(ip_address);
+
+            var responseString = "";
+            var oResults = new ZeroBounceResultsModel();
+            System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)WebRequest.Create(apiUrl);
+            request.Timeout = RequestTimeOut;
+            request.Method = "GET";
+            Console.WriteLine("Input APIKey: " + api_key);
+          
+            var serializer = new JavaScriptSerializer();
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    response.GetResponseStream().ReadTimeout = ReadTimeOut;
+                    using (StreamReader ostream = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseString = ostream.ReadToEnd();
+                        oResults = serializer.Deserialize<ZeroBounceResultsModel>(responseString);                        
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                if (ex.Message.Contains("The operation has timed out")) oResults.sub_status = "timeout_exceeded";
+                else oResults.sub_status = "exception_occurred";
+                oResults.status = "Unknown";
+                oResults.domain = EmailToValidate.Substring(EmailToValidate.IndexOf("@") + 1).ToLower();
+                oResults.account = EmailToValidate.Substring(0, EmailToValidate.IndexOf("@")).ToLower();
+                oResults.address = EmailToValidate.ToLower();
+            }
+            return oResults;
+        }
+        public ZeroBounceCreditsModel GetCredits()
+        {
+            // secure SSL / TLS channel for different .Net versions            
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11;
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+
+            var apiUrl = "https://api.zerobounce.net/v2/getcredits?api_key=" + api_key;
+            var responseString = "";
+            var oResults = new ZeroBounceCreditsModel();
+
+            System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)WebRequest.Create(apiUrl);
+            request.Timeout = RequestTimeOut;
+            request.Method = "GET";           
+            Console.WriteLine("APIKey: " + api_key);
+
+            var serializer = new JavaScriptSerializer();
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    response.GetResponseStream().ReadTimeout = ReadTimeOut;
+                    using (StreamReader ostream = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseString = ostream.ReadToEnd();
+                        oResults = serializer.Deserialize<ZeroBounceCreditsModel>(responseString);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Add Your Error Loggin
+            }
+            return oResults;
+        }
+    }
+}
